caa7ce56be2dec8159a3c5f93faa602395b1b67f
Ian McBurnie <ianmcburnie@hotmail.com>
2017-11-24 20:43:32 -0800

    0.0.2

--- test/static/bundle-module.js
+++ test/static/bundle-module.js
@@ -1,4 +1,4 @@
-$_mod.installed("makeup-navigation-emitter$0.0.1", "custom-event-polyfill", "0.3.0");
+$_mod.installed("makeup-navigation-emitter$0.0.2", "custom-event-polyfill", "0.3.0");
 $_mod.main("/custom-event-polyfill$0.3.0", "custom-event-polyfill");
 $_mod.def("/custom-event-polyfill$0.3.0/custom-event-polyfill", function(require, exports, module, __filename, __dirname) { // Polyfill for creating CustomEvents on IE9/10/11
 
@@ -46,7 +46,7 @@ try {
 }
 
 });
-$_mod.def("/makeup-navigation-emitter$0.0.1/util", function(require, exports, module, __filename, __dirname) { "use strict";
+$_mod.def("/makeup-navigation-emitter$0.0.2/util", function(require, exports, module, __filename, __dirname) { "use strict";
 
 function nodeListToArray(nodeList) {
     return Array.prototype.slice.call(nodeList);
@@ -57,7 +57,7 @@ module.exports = {
 };
 
 });
-$_mod.installed("makeup-navigation-emitter$0.0.1", "makeup-key-emitter", "0.0.2");
+$_mod.installed("makeup-navigation-emitter$0.0.2", "makeup-key-emitter", "0.0.2");
 $_mod.main("/makeup-key-emitter$0.0.2", "");
 $_mod.installed("makeup-key-emitter$0.0.2", "custom-event-polyfill", "0.3.0");
 $_mod.def("/makeup-key-emitter$0.0.2/util", function(require, exports, module, __filename, __dirname) { 'use strict';
@@ -169,7 +169,7 @@ module.exports = {
 };
 
 });
-$_mod.installed("makeup-navigation-emitter$0.0.1", "makeup-exit-emitter", "0.0.2");
+$_mod.installed("makeup-navigation-emitter$0.0.2", "makeup-exit-emitter", "0.0.2");
 $_mod.main("/makeup-exit-emitter$0.0.2", "");
 $_mod.installed("makeup-exit-emitter$0.0.2", "custom-event-polyfill", "0.3.0");
 $_mod.def("/makeup-exit-emitter$0.0.2/index", function(require, exports, module, __filename, __dirname) { 'use strict';
@@ -233,7 +233,7 @@ module.exports = {
 };
 
 });
-$_mod.def("/makeup-navigation-emitter$0.0.1/index", function(require, exports, module, __filename, __dirname) { 'use strict';
+$_mod.def("/makeup-navigation-emitter$0.0.2/index", function(require, exports, module, __filename, __dirname) { 'use strict';
 
 // requires Object.assign polyfill or transform for IE
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
@@ -248,7 +248,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Util = require('/makeup-navigation-emitter$0.0.1/util'/*'./util.js'*/);
+var Util = require('/makeup-navigation-emitter$0.0.2/util'/*'./util.js'*/);
 var KeyEmitter = require('/makeup-key-emitter$0.0.2/index'/*'makeup-key-emitter'*/);
 var ExitEmitter = require('/makeup-exit-emitter$0.0.2/index'/*'makeup-exit-emitter'*/);
 var dataSetKey = 'data-makeup-index';
